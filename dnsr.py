697e5153868e61cde844c44a7c15e88275747391
Lawrence Latif <public@lawrencelatif.com>
2021-06-03 19:19:32 -0700

    Added argparse, minor comments

--- dnsr.py
+++ dnsr.py
@@ -1,12 +1,28 @@
+import argparse
 from dns.resolver import dns
 
+parser = argparse.ArgumentParser()
+parser.add_argument("host", help="A fully qualified hostname to test DNS")
+
+## A list of name servers that we want to test
 nameservers = ['172.16.0.1', '172.16.0.2']
 
-resolver = dns.resolver.Resolver()
-for ns in nameservers:
-  
-  resolver.nameservers = [ns]
-  result = resolver.resolve('fred.lon.bnew.org', 'A')
+def nsCheck(host):
 
-  for ipval in result:
-    print('Nameserver', result.nameserver, '\nIP', ipval.to_text())
+  resolver = dns.resolver.Resolver()
+  for ns in nameservers:
+
+    ## Manually set the nameserver used to resolve queries from the list 
+    resolver.nameservers = [ns]
+    result = resolver.resolve(host, 'A')
+
+    for ipval in result:
+      ## TODO: send results to a database
+      print('Nameserver', result.nameserver, '\nIP', ipval.to_text())
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    args = parser.parse_args() 
+    
+    nsCheck(args.host)
